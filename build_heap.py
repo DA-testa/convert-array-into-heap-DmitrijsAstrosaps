5d84c505e16482bae342d436139f1ec3512c2792
Sheepy <125596139+DmitrijsAstrosaps@users.noreply.github.com>
2023-03-19 19:31:35 +0000

    commit.build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,28 +1,54 @@
-# python3
+# python3Arr
 
 
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
+    n=len(data)
+    for i in range(n//2-1,-1,-1):
+        sort_Butt(data,i,swaps)
 
     return swaps
 
+def sort_Butt(data, i, swaps):
+    n=len(data)
+    MI = i
+    LCh = 2*i+1
+    RCH = 2*i+2
+
+    if(RCH<=n-1 and data[RCH]<data[MI]):
+        MI = RCH
+    if(LCh<=n-1 and data[LCh]<data[MI]):
+        MI = LCh
+    if(MI!=i):
+        swaps.append((i,MI))
+        data[i],data[MI]=data[MI],data[i]
+        sort_Butt(data,MI,swaps)
 
 def main():
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    Che = input()
+    if "I" in Che:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        swaps = build_heap(data)
+    elif "F" in Che:
+        FN = input()
+        if "a" in FN:
+            return
+        else:
+            with open("./tests/"+FN,encoding = 'utf-8') as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+                assert len(data) == n
+                swaps = build_heap(data)
+    else:
+        return
 
     # calls function to assess the data 
     # and give back all swaps
